539c0bb0f2a37d55e55489c81da6db2793ba2356
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-02-18 23:27:26 +0100

    Added the `apis` APP to the installed apps list

--- library/library/settings.py
+++ library/library/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party APPs
+    "rest_framework",
     # local APPs
     "books.apps.BooksConfig",
+    "apis.apps.ApisConfig",
 ]
 
 MIDDLEWARE = [
