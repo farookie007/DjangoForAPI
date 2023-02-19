904ec61db7609c54d2824e4c3c81182e5a96fc9f
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-02-19 01:13:02 +0100

    Added the `Todo` model to the admin page and tweaked how it is rendered

--- todo/todos/admin.py
+++ todo/todos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+# Local imports
+from .models import Todo
 
 # Register your models here.
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "body",
+    )
