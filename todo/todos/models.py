b383bda90af38a5e0dd56579e839b5ae6d0de467
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-02-19 01:03:37 +0100

    Created the `Todo` model

--- todo/todos/models.py
+++ todo/todos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    """A model representing each Todo object on the database."""
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+
+    def __str__(self):
+        """String represetation of the model.
+        Returns:
+            str: the title of the object."""
+        return self.title
