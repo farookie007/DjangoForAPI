98ce1314cc6e39299e64d4dbec5645a2d490a39b
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-06-01 02:47:46 +0100

    Updated the Admin Page
    Created the `CustomUserAdmin` class to register the `CustomUser` model
    Added both the `CustomUserAdmin` and the `CustomUser` model to the admin page

--- blogapi/accounts/admin.py
+++ blogapi/accounts/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+# Local imports
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = [
+        "email",
+        "username",
+        "name",
+        "is_staff",
+    ]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {
+            'fields': (
+                "name",
+            ),
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {
+            'fields': (
+                "name",
+            ),
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
