569d1547ffb9d923631aee68992ea145fd07ffcf
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-06-03 00:50:13 +0100

    Added an alternative way of getting the user model

--- blogapi/posts/models.py
+++ blogapi/posts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from django.conf import settings
+from django.conf import settings# [alternatively]from django.contrib.auth import get_user_model
 
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField()
+    # [alternatively]author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
