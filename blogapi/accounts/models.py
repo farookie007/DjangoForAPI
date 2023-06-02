6add290ad38d5148a30a8e500380c153710e4ad3
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-06-02 12:42:14 +0100

    Deleted  project so as to practice by implementing with no help

--- blogapi/accounts/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-# Create your models here.
-class CustomUser(AbstractUser):
-    name = models.CharField(max_length=100, null=True, blank=True)
