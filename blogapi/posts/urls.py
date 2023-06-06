b8c87347da7fd52bb282d744b7e3356ce4cbdd2f
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-06-06 20:20:20 +0100

    Created the urlpatterns for the `post_detail` and `post_list` view

---
+++ blogapi/posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import PostList, PostDetail
+
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('', PostList.as_view(), name='post_list'),
+    path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
+]
