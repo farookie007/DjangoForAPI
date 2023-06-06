de388ccb7c0f28bf5cb924260f8d86a40eca0ab3
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-06-06 17:57:50 +0100

    Created the `accounts` app

--- blogapi/accounts/admin.py
+++ blogapi/accounts/admin.py
@@ -1,34 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-# local imports
+
 from .models import CustomUser
-from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    form = CustomUserChangeForm
-    add_form = CustomUserCreationForm
-    list_display = [
-        "email",
-        "username",
-        "name",
-        "is_staff",
-    ]
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {
-            'fields': (
-                "name",
-            ),
-        }),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {
-            'fields': (
-                "name",
-            ),
-        }),
-    )
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser)
--- blogapi/accounts/models.py
+++ blogapi/accounts/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    name = models.CharField(max_length=100, null=True, blank=True)
+    name = models.CharField(max_length=50, null=True, blank=True)
