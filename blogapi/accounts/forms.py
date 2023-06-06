9a84fda221cfd261e8c3aa7692b1ef9442a37074
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-06-06 18:17:53 +0100

    Corrrected error in code

--- blogapi/accounts/forms.py
+++ blogapi/accounts/forms.py
@@ -6,10 +6,10 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.fields + ("name",)
+        fields = UserCreationForm.Meta.fields + ("name",)
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.fields
+        fields = UserChangeForm.Meta.fields
