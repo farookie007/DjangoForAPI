0dbce1091a08d057d3f846d5af6be44d5c37ad56
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-06-06 18:18:54 +0100

    Modified the admin page
    Added the `CustomUserAdmin` class to modify the admin page
    Registered the `CustomUser` model and `CustomUserAdmin` class on the admin portal

--- blogapi/accounts/admin.py
+++ blogapi/accounts/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display = [
+        'username',
+        'email',
+        'name',
+        'is_staff',
+    ]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {
+            'fields': (
+                'name',
+            ),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'fields': (
+                'name',
+            ),
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
