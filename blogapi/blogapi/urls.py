7799c26c31c4f620cf77454d49c2c020bae48288
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-06-08 18:06:49 +0100

    Corrected "/" ommited in urls config

--- blogapi/blogapi/urls.py
+++ blogapi/blogapi/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1', include('posts.urls', namespace='posts')),
+    path('api/v1/', include('posts.urls', namespace='posts')),
 ]
