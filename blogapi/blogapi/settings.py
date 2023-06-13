0316ec09b86ec79d819b83d9c0b6f58cd63b0d3f
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-06-14 00:02:47 +0100

    Updated the project settings for the `django-allauth` package
    Added `allauth` app and its components to the `INSTALLED_APPS` list
    Added `dj_rest_auth.registration` to the `INSTALLED_APPS` list
    Updated the `context_processors` option in the `TEMPLATES` list
    Set the email backend to use django `console.EmailBackend`
    Set the `SITE_ID` to 1

--- blogapi/blogapi/settings.py
+++ blogapi/blogapi/settings.py
@@ -37,12 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # 3rd-party apps,
     'rest_framework',
     'corsheaders',
     'rest_framework.authtoken',
     'dj_rest_auth',
+    'dj_rest_auth.registration',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     
     # local apps,
     'accounts.apps.AccountsConfig',
@@ -74,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
@@ -134,6 +140,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Email backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Site ID
+SITE_ID = 1
 
 # Custom user model
 AUTH_USER_MODEL = "accounts.CustomUser"
