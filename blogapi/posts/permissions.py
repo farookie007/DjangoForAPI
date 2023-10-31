8e3e200769dfa9906a9d038ddeb847946da08ed3
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-10-31 16:57:13 +0100

    🦋: create `IsAuthorOrReadOnly` permission

--- blogapi/posts/permissions.py
+++ blogapi/posts/permissions.py
@@ -3,13 +3,14 @@ from rest_framework import permissions
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        # Authenticated users only can see list view
+        # handles permission for POST and the SAFE methods
+        # only authenticated users cant access listview
         return request.user.is_authenticated
     
     def has_object_permission(self, request, view, obj):
-        # Read permissions are only allowed to GET, HEAD, or OPTIONS requests
-        if request.method in permissions.SAFE_METHODS:
+        # handles permissions for all SaFE methods and other methods
+        # except POST method
+        if request.method  in permissions.SAFE_METHODS:
             return True
-        
-        # Write permissions are only allowed to the author of a post
-        return obj.author == request.user
+        # Write permissions are only allowed for the author of the post
+        return request.user == obj.author
