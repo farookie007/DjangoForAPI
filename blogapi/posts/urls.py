5448e58d00cb9f015f948deacbe96550f2ad0ec5
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-10-31 16:58:02 +0100

    🦋: changed the urls to routers

--- blogapi/posts/urls.py
+++ blogapi/posts/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import PostList, PostDetail, UserList, UserDetail
+from .views import PostViewSet, UserViewSet
 
 
-app_name = 'posts'
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
 
-urlpatterns = [
-    path('', PostList.as_view(), name='post_list'),
-    path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
-    path('users/', UserList.as_view(), name='user_list'),
-    path('users/<int:pk>/', UserDetail.as_view(), name='user_detail'),
-]
+urlpatterns = router.urls
