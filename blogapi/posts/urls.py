60309e0b1b1bc732e815818dcd09c4679ee24d46
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-10-31 16:03:08 +0100

    🦋: added urlpatterns for the `User` model listview and detailview

--- blogapi/posts/urls.py
+++ blogapi/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, UserList, UserDetail
 
 
 app_name = 'posts'
@@ -8,4 +8,6 @@ app_name = 'posts'
 urlpatterns = [
     path('', PostList.as_view(), name='post_list'),
     path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
+    path('users/', UserList.as_view(), name='user_list'),
+    path('users/<int:pk>/', UserDetail.as_view(), name='user_detail'),
 ]
