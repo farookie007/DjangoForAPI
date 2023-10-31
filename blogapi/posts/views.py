05fd35cddbabe2b612dcfbef775c0c2717f7b772
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-10-31 16:57:40 +0100

    🦋: changed the view classes to viewsets

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,26 +1,15 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+from rest_framework import viewsets
 
 from .models import Post
 from .serializers import PostSerializer, UserSerializer
 
 
-class PostList(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class UserList(generics.ListCreateAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-
