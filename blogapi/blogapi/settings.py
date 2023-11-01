1acb8df17a2f5035f4a6991d890d7614d4500f3e
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-11-01 14:06:20 +0100

    🐱‍👤: added `drf_spectacular` to INSTALLED_APPS and its config

--- blogapi/blogapi/settings.py
+++ blogapi/blogapi/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'drf_spectacular',
     
     # local apps,
     'accounts.apps.AccountsConfig',
@@ -157,7 +158,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS': "drf_spectacular.openapi.AutoSchema",
 }
 
 # CORS settings
@@ -170,3 +172,10 @@ CORS_ORIGIN_WHITELIST = [
 CSRF_TRUSTED_ORIGINS = [
     'https://127.0.0.1:3000',
 ]
+
+# Spectacular settings
+SPECTACULAR_SETTINGS = {
+    'TITLE': "Blog API Project",
+    'DESCRIPTION': "A Sample Blog to Learn About DRF",
+    'VERSION': "1.0.0"
+}
